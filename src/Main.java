73c0b2086e7c6daf933c854ce16eb9b6940be882
jaluque <jaluque@atsistemas.com>
2017-06-06 10:10:12 +0200

    cambios rama4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        String a  = "polloman";
         System.out.println("Hello World!");
 
         System.out.println("cambios en la rama newBranch1");
