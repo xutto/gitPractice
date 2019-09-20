d235abab4974af07723a5a0db76e697876ffcda2
jaluque <jaluque@atsistemas.com>
2019-09-20 14:39:45 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,6 +17,24 @@ public class Main {
 
         System.out.println("cambios en la rama3");
         System.out.println("otro cambio en la rama3");
+
+        System.out.println(a);
+
+        List<Persona> list = new ArrayList<>();
+
+        Persona p1 = new Persona();
+        p1.setNombre("Luis");
+        list.add(p1);
+
+        Persona p2 = list.get(0);
+        p2.setNombre("Juan");
+
+        Persona p3 = list.get(0);
+
+
+        System.out.println(p3.getNombre());
+
+
 
         System.out.printf("cambio para ver si va con proxy");
 
---
+++ src/Persona.java
@@ -0,0 +1,15 @@
+/**
+ * Created by jaluque on 15/06/2017.
+ */
+public class Persona {
+
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
